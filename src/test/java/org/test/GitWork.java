739d19e7fa7d296edf085e7c0a257a19a4b08907
anand130188 <srisaicommunication2021@gmail.com>
2023-07-31 17:08:25 +0530

    First time Push

---
+++ src/test/java/org/test/GitWork.java
@@ -0,0 +1,22 @@
+package org.test;
+
+public class GitWork {
+	
+	public void tamilnadu() {
+		System.out.println("Chennai");
+
+	}
+	public void Kerala() {
+		System.out.println("cochin");
+
+	}
+	public void Andhra() {
+		System.out.println("Hydrabad");
+
+	}
+	public void Karnataka() {
+		System.out.println("Bengaluru");
+
+	}
+
+}
